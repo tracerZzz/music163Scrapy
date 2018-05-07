76074d99bcf8cdb93b2eab67d0ad02b8382e77a6
tracerzzz <173439618@qq.com>
2018-05-07 09:55:35 +0800

    增加最新歌单爬取

--- scrapyMusic163/spiders/music163Spider.py
+++ scrapyMusic163/spiders/music163Spider.py
@@ -11,6 +11,7 @@ from scrapyMusic163.items import Scrapymusic163Item
 from scrapy_splash import SplashRequest
 # from ..settings import DEFAULT_REQUEST_HEADERS
 import parsel
+
 # import codecs
 
 class Music163Spider(scrapy.Spider):
@@ -18,13 +19,15 @@ class Music163Spider(scrapy.Spider):
     name = "music163"
     allowed_domains = ["163.com"]
     start_urls = [
-        "https://music.163.com"
+        "https://music.163.com/discover/playlist/?order=new",
+        "https://music.163.com/#/discover/playlist/"
+
     ]
     count =0
 
     def parse(self, response):
         for url in self.start_urls:
-            yield SplashRequest(url+('/#/discover/playlist/'), self.parseRequest,endpoint='render.json', args={'wait': 1,'html':1,'iframes': 1,'script':1})
+            yield SplashRequest(url=url, callback=self.parseRequest,endpoint='render.json', args={'wait': 1,'html':1,'iframes': 1,'script':1})
 
     def parseRequest(self,response):
         iframe_html = response.data['childFrames'][0]['html']
@@ -35,7 +38,7 @@ class Music163Spider(scrapy.Spider):
             item['title'] = se.xpath('text()')[0].extract()
             item['link'] = se.xpath('@href')[0].extract()
             url = response.urljoin(item['link'])
-            yield SplashRequest(url,self.parse_gedan,endpoint='render.json', args={'wait': 3,'html':1,'iframes': 1,'script':1})
+            yield SplashRequest(url,self.parse_gedan,endpoint='render.json', args={'wait': 2,'html':1,'iframes': 1,'script':1})
     def parse_gedan(self, response):
         iframe_html = response.data['childFrames'][0]['html']
         sel = parsel.Selector(iframe_html)
@@ -48,16 +51,13 @@ class Music163Spider(scrapy.Spider):
             item['listenCount']=se.xpath('div/div/span[@class="nb"]/text()')[0].extract()
             if(item['listenCount'] and "万" in item['listenCount']):
                 item['listenCount']=int(item['listenCount'].replace("万","0000"))
+            item['listenCount']=int(item['listenCount'])
             item['imgUrl']=se.xpath('div/img[@class="j-flag"]/@src')[0].extract()
             yield item
         next_pages = sel.xpath('//a[@class="zbtn znxt"]')
 
         if next_pages:
             url = response.urljoin(next_pages.xpath('@href')[0].extract())
-            yield SplashRequest(url,self.parse_gedan,endpoint='render.json', args={'wait': 3,'html':1,'iframes': 1,'script':1})
+            yield SplashRequest(url,self.parse_gedan,endpoint='render.json', args={'wait': 2,'html':1,'iframes': 1,'script':1})
 if __name__ == '__main__':
-    # os.system("scrapy runspider music163Spider.py -o result.json")
-    name = 'music163'
-    cmd = 'scrapy crawl {0} -o result.json'.format(name)
-    cmdline.execute(cmd.split())
     pass
